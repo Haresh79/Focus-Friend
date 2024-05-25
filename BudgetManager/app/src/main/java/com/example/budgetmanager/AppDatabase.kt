72771d48a606317b18cc35607a1b7edaa9e761af
Haresh Panigrahi <121621310+Haresh79@users.noreply.github.com>
2024-05-25 22:14:25 +0530

    Delete BudgetManager directory

--- BudgetManager/app/src/main/java/com/example/budgetmanager/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.budgetmanager
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = arrayOf(Transection::class), version = 1)
-abstract class AppDatabase:RoomDatabase() {
-    abstract fun transectionDao():transectionDAO
-}
